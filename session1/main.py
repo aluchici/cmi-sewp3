d583bf4962831122020b001c86d245488eeb32d0
Andrei <andrei.luchici@daciandata.tech>
2022-03-09 11:49:11 +0200

    session1

---
+++ session1/main.py
@@ -0,0 +1,25 @@
+t = True
+f = False
+a = 10
+b = 43.54
+c = "exemplu"
+d = 'alt exemplu'
+
+e = [10, 20, 'exemplu']
+f = (10, 20, 'exemplu')
+
+g = {
+    "name": "cmi",
+    1: 34.54,
+    "legume": ["rosii", "castraveti", "varza"],
+    "note": [
+        {
+            "titlu": "nota 1",
+            "desc": "khjdsfgkldjgkljfdlk"
+        },
+        {
+            "title": "l;dfjkdj",
+            "description": "fodsjflkjd"
+        }
+    ]
+}
